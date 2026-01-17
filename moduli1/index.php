55839c3f070d8efbdfbf31c38ca15d0442810699
bleardavdullahu <bleardavdullahu7@gmail.com>
2026-01-17 14:49:06 +0100

    index.php

---
+++ moduli1/index.php
@@ -0,0 +1,22 @@
+<?php
+ echo("This is my first time using php");
+ 
+    $school_Name = "Digital School";
+
+    echo ("i go to $school_Name")."<hr>";
+
+    $numri = 2;
+
+    echo ("i am in level number $numri")."<hr>";
+
+    $the_String = "ju jeni klasa ma e mira";
+
+    echo strlen($the_String)."<hr>";
+
+    echo str_word_count($the_String)."<hr>";
+    
+    echo str_replace("mira", "zhurmshja", $the_String)."<hr>";
+
+    echo strrev($the_String)."<hr>";
+    
+ ?>
